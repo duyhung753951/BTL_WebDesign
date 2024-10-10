6a2862c6d88d84276acb5176d42be5c776ffeb12
TranDHung <duyhung160605@gmail.com>
2024-10-10 21:25:42 +0700

    update favicon, logo, ten web, trải nghiệm Slider image

--- js/slider.js
+++ js/slider.js
@@ -2,13 +2,52 @@ let scrollContainer = document.querySelector(".gallery");
 let preBtn = document.getElementById("preBtn");
 let nextBtn = document.getElementById("nextBtn");
 const scrollAmount = scrollContainer.clientWidth;
-    
-nextBtn.addEventListener('click', () => {
-    scrollContainer.scrollLeft += scrollAmount;
-    scrollContainer.style.scrollBehavior = "smooth";
-});
 
-preBtn.addEventListener('click', () => {
-  scrollContainer.scrollLeft -= scrollAmount;
+var counter = 0;
+
+// Số lượng ảnh trong gallery
+const totalItems = scrollContainer.scrollWidth / scrollContainer.clientWidth;
+
+nextBtn.addEventListener('click', nextSlide);
+function nextSlide(){
   scrollContainer.style.scrollBehavior = "smooth";
+  
+  // Kiểm tra nếu là ảnh cuối thì trở về ảnh đầu
+  if(counter >= totalItems - 1){
+    scrollContainer.scrollLeft = 0; // Quay về đầu
+    counter = 0; // Reset counter
+  }
+  else {
+    scrollContainer.scrollLeft += scrollAmount;
+    counter++;
+  }
+}
+
+preBtn.addEventListener('click', previousSlide);
+function previousSlide(){
+  scrollContainer.style.scrollBehavior = "smooth";
+  
+  // Kiểm tra nếu là ảnh đầu thì nhảy tới ảnh cuối
+  if(counter <= 0){
+    scrollContainer.scrollLeft = scrollContainer.scrollWidth; // Nhảy đến cuối
+    counter = totalItems - 1; // Set counter về cuối
+  }
+  else {
+    scrollContainer.scrollLeft -= scrollAmount;
+    counter--;
+  }
+}
+
+function autoSliding(){
+  deleteInterval = setInterval(timer, 4000);
+  function timer(){
+    nextSlide(); 
+  }
+}
+autoSliding();
+
+// Dừng cuộn tự động khi di chuột vào
+const container = document.querySelector(".gallery-wrap");
+container.addEventListener('mouseover', function(){
+  clearInterval(deleteInterval);
 });
